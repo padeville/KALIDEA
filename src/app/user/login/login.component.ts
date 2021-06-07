fa8805077a1461c64d4356c951722ee44f3ccf88
bropad <pierre.a.deville@gmail.com>
2021-06-07 18:31:58 +0200

    login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable, of, Subscription, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { User } from '../models/user';
+import { UserService } from '../user.service';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
+  profileForm!: FormGroup;
+  userObserbable!: Subscription;
 
-  constructor() { }
+  credentialError = false;
+  constructor(
+    private userService: UserService,
+    private location: Location,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.profileForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
+  }
+  onSubmit() {
+    this.userObserbable = this.userService
+      .login(this.profileForm.value)
+      .pipe(
+        catchError((err) => {
+          this.credentialError = true;
+          console.log('Handling error locally and rethrowing it...', err);
+          return of(null);
+        })
+      )
+      .subscribe((user) => {
+        console.log(user);
+        if (user !== null) {
+          console.log(user);
+
+          this.router.navigate(['']);
+        }
+      });
   }
 
+  get email() {
+    return this.profileForm.get('email')!;
+  }
+  get password() {
+    return this.profileForm.get('password')!;
+  }
+
+  ngOnDestroy(): void {
+    this.userObserbable?.unsubscribe();
+  }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { UserRoutingModule } from './user-routing.module';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    LogoutComponent
-  ],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [LoginComponent, LogoutComponent],
+  imports: [CommonModule, UserRoutingModule, ReactiveFormsModule, FormsModule],
 })
-export class UserModule { }
+export class UserModule {}
