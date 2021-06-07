b2686e2de21a565cd967010e575deb0db1dd6f7e
bropad <pierre.a.deville@gmail.com>
2021-06-07 23:22:27 +0200

    on s'arrete la pour ce soir

--- src/app/shared/form-field/form-field.component.ts
+++ src/app/shared/form-field/form-field.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { FormFildInputDirective } from '../form-fild-input.directive';
+import { FormFieldInputDirective } from './form-field-input.directive';
 
 @Component({
   selector: 'app-form-field',
   templateUrl: './form-field.component.html',
-  styleUrls: ['./form-field.component.sass']
+  styleUrls: ['./form-field.component.sass'],
 })
 export class FormFieldComponent implements OnInit {
+  @ViewChildren (FormFildInputDirective) formFieldDirective!: FormFildInputDirective;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    console.log(this.formFieldDirective)
+    this.formFieldDirective
+  }
 }
